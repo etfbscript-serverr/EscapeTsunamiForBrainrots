0bbd093f99d895be263fae3dd82c2cc334003411
etfbscript-serverr <chantalcolter@gmail.com>
2026-03-01 22:48:34 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/5OyVn71D/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/EjQb3tu1/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
